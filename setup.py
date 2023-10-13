f77f6a9e2da64dec9bfd418b9908e6f707000832
baskina <katyabaskina@gmail.com>
2023-10-13 14:40:12 +0200

    Add package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='personal assistant',
+      version='1',
+      description='Program includes features: address book; notes; sorting files',
+      url='https://github.com/Baskina/personal_assistant',
+      author='Group',
+      author_email='baskina.development@gmail.com',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['start-bot = stc.main:main']},
+      )
