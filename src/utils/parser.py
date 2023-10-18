117d4719937d832d8a4592b4b6effcf419541dbe
baskina <katyabaskina@gmail.com>
2023-10-18 16:41:42 +0200

    - change cutoff in get_close_matches method

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -6,7 +6,7 @@ from .constants import BOT_COMMANDS
 def parser(line):
     possible_command = ' '.join(line.split(' ')[:2])
     data = line[len(possible_command) + 1:]
-    command = difflib.get_close_matches(possible_command, BOT_COMMANDS, n=1, cutoff=0.6)
+    command = difflib.get_close_matches(possible_command, BOT_COMMANDS, n=1, cutoff=0.7)
     if not len(command):
         raise AttributeError
     data = data.strip().split(' ') if len(data) > 0 else None
